a361312a590234ce2917e4c9afc4d039afad69da
= <syedahmed.csse@hotmail.com>
2022-04-27 13:47:23 +0530

    websocket app working; tokens hidden

--- slackbot/src/main/java/com/syed/test/slackbot/FirstBot.java
+++ slackbot/src/main/java/com/syed/test/slackbot/FirstBot.java
@@ -8,17 +8,29 @@ import com.slack.api.model.event.MessageEvent;
 
 public class FirstBot {
     public static void main(String str[]) throws Exception {
-        String botToken = "xoxb-hidden";
-        String appToken = "xapp-1-hidden";
+        String botToken = "xoxb-hid"; //syed app
+        //String botToken = "xoxb-hid"; //mani app
+        String appToken = "xapp-1-hid"; //syed app
+        //String appToken = "xapp-1-hid"; //maniapp
         App app = new App(AppConfig.builder().singleTeamBotToken(botToken).build());
         app.event(MessageEvent.class, (req, ctx) -> {
+            System.out.println("message event tracked\n\n");
             MessageEvent event = req.getEvent();
-            ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
-                    .channel(event.getChannel())
-                    .threadTs(event.getTs())
-                    .text("<@" + event.getUser() + "> Thank you! We greatly appreciate your efforts :two_hearts:"));
-            if (!message.isOk()) {
-                ctx.logger.error("chat.postMessage failed: {}", message.getError());
+            String msg = event.getText();
+            System.out.println("msg:\n" + msg + "\n");
+            if (msg.startsWith("New Deal Signed")) {
+                System.out.println("yay, new deal signed");
+                ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
+                        .channel(event.getChannel())
+                        .threadTs(event.getTs())
+                        .text("Yay deal sealed. Consider sending the following materials to the customer:\n\t" +
+                                "<https://www.snaplogic.com/blog/using-snaplogic-at-snaplogic-user-onboarding|User Onboarding>\n\t" +
+                                "<https://www.snaplogic.com/resources|Welcome Kit>"));
+                if (!message.isOk()) {
+                    ctx.logger.error("chat.postMessage failed: {}", message.getError());
+                } else {
+                    System.out.println("sent successfully!!");
+                }
             }
             return ctx.ack();
         });
